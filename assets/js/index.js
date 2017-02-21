f6d56786aacfd02ccf46c14c2e00535bbbbf1ab6
vivek16goyal <vivek.umredkar@goyalonline.com>
2017-02-21 12:27:34 +0530

    s

--- assets/js/index.js
+++ assets/js/index.js
@@ -34,8 +34,8 @@ var APPType = "$";
 
 
 //  3)///////*******A to Z Dava [Demo Version Given To Drug Deal] **********************//////////
-var Heading = "Drug Deal";
-var PCODE = "00256A";
+var Heading = "";
+var PCODE = "DEM010";
 var folderPathImg = "file:///storage/sdcard0/" + PCODE + "/";
 var DownloadPath = "file:///storage/sdcard0/";
 var FolderName = PCODE;
